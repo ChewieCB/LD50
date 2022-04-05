96d47f961408f01107997994140380846fcca404
Jack McCaffrey <jackmccaffrey96@gmail.com>
2022-04-05 10:23:00 +0100

    Main game music now starts playing on load of main scene

--- src/bgm/bgm_player.gd
+++ src/bgm/bgm_player.gd
@@ -11,13 +11,13 @@ onready var animation_player = $AnimationPlayer
 func _ready():
 	animation_player.play("off")
 	menu_music_player.play()
-	game_music_player.play()
 
 func play_menu():
 	animation_player.play("fade_in_menu")
 
 
 func menu_to_game():
+	game_music_player.play()
 	animation_player.play("crossfade_menu_to_game")
 
 
